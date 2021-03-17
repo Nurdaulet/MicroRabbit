7ba316e775236fd2775de68b0598d8f8b1d9f5c4
nurdaulet <nurdaulet1312@gmail.com>
2021-03-17 08:01:34 +0400

    Implemented Interfaces and classes for Domain Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,17 @@
+﻿
+
+using MediatR;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+
+        public string MessageType { get; protected set; }
+
+    }
+}
